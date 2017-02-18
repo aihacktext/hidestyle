5203d4036be05793a6a6e3ae15bbaf75693aab32
Federico Ceratto <federico.ceratto@gmail.com>
2017-02-18 20:12:04 +0000

    Webapp integration, cleanup

--- src/classification/classifier.py
+++ src/classification/classifier.py
@@ -1,47 +1,44 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Text classifier
 #
-#
-import gzip
-from optparse import OptionParser
+
 from contextlib import contextmanager
-from sklearn.svm import LinearSVC
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cross_validation import train_test_split
-from time import time
-from sklearn import metrics
-import os.path
-
-import requests
-
-from sklearn.externals import joblib
-
-from sklearn.pipeline import Pipeline
-
 from nltk.classify.scikitlearn import SklearnClassifier
-
-classif = SklearnClassifier(LinearSVC())
-
+from lime.lime_text import LimeTextExplainer
+from optparse import OptionParser
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+from sklearn.externals import joblib
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.naive_bayes import MultinomialNB
-
+from sklearn.neighbors import DistanceMetric
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import LinearSVC
+from time import time
+import numpy as np
+import os.path
+import scipy
+import requests
 
 @contextmanager
 def timer(before, after=None):
     before = before.capitalize()
-    print "%s..." % before
+    print("%s..." % before)
     t = time()
 
     yield
 
     if after is None:
-        after = "%s done" % before
+        after =("%s done" % before)
     else:
         after = after.capitalize()
 
-    print "%s in %.3fs" % (after, time() - t)
+    print("%s in %.3fs" % (after, time() - t))
 
 def download_if_needed(url, fname):
     if os.path.isfile(fname):
@@ -64,14 +61,14 @@ def parse_args():
     return opts
 
 def show_most_informative_features(vectorizer, clf, n=20):
-    print 'Most informative features:'
+    print('Most informative features:')
     feature_names = vectorizer.get_feature_names()
     coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
     top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
     for (coef_1, fn_1), (coef_2, fn_2) in top:
-        print "\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2)
+        print("\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2))
 
-    print '--'
+    print("--")
 
 def learn():
     """Learn to classify texts by author
@@ -79,13 +76,20 @@ def learn():
     X = []
     y = []
 
+    seen_authors = {}
     with timer('parsing imdb'):
         with open('imdb62.txt') as f:
-            cnt = 0
             for line in f:
                 tok = line.split(None, 5)
                 author = tok[1]
                 body = tok[5]
+                try:
+                    if seen_authors[author] == 50:
+                        continue
+                    seen_authors[author] += 1
+                except KeyError:
+                    seen_authors[author] = 1
+
                 X.append(body)
                 y.append(author)
 
@@ -105,8 +109,15 @@ def learn():
     Xo = X
     yo = y
     with timer('fitting', 'fitting done'):
+        #vectorizer = CountVectorizer(
+        #    analyzer='word',
+        #    ngram_range=(1, 1),
+        #    max_features=10000,
+        #    stop_words='english'
+        #)
         vectorizer = TfidfVectorizer(
             stop_words='english',
+            #ngram_range=(1, 3),
             lowercase=True,
         )
         X = vectorizer.fit_transform(X)
@@ -114,7 +125,7 @@ def learn():
 
     with timer('splitting'):
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.1, random_state=42)
+            X, y, test_size=0.10, random_state=42)
 
     print("total: n_samples: %d, n_features: %d" % X.shape)
     print("train: n_samples: %d, n_features: %d" % X_train.shape)
@@ -122,6 +133,7 @@ def learn():
 
     #clf = LinearSVC(loss='l2', penalty='l2', dual=False, tol=1e-3)
     clf = LinearSVC()
+    clf = LogisticRegression()
 
     with timer('training'):
         clf.fit(X_train, y_train)
@@ -133,15 +145,115 @@ def learn():
     show_most_informative_features(vectorizer, clf, n=45)
     print(clf.decision_function(X_train))
 
-    #with timer('Measuring confidence'):
-    #    decision = clf.decision_function(X_train)
-    #    clf._max_confidence = max(decision)
-    #    clf._min_confidence = min(decision)
-    clf._max_confidence = 1.0
-    clf._min_confidence = 0.0
+    num_examples_per_class = 50
+    user_input = "..................."
+    user_features = vectorizer.transform([user_input])
+    duplicated_user_features = scipy.sparse.vstack([user_features for _ in range(int(num_examples_per_class / user_features.shape[0]))])
 
+    #reviews = X
+    #rand_indices = np.random.permutation(range(len(reviews)))[:args.num_examples_per_class]
+    #reviews = [review for (i, review) in enumerate(reviews) if i in rand_indices]
+    rand_review_features = X
+    #vectorizer.fit_transform(X)
+    print('Review features shape:', rand_review_features.shape)
+
+    ###
+
+    #  rand_review_features = X
+    #  duplicated_user_features = y
+    #  #train_data = scipy.sparse.vstack([rand_review_features, duplicated_user_features])
+    #  #print('Train data shape:', train_data.shape)
+
+
+    #  user_features = vectorizer.transform([user_input])
+    #  # Note: We could split the user review into more sentences to get more data, but this does not seem to help.
+    #  # user_features = vectorizer.transform(split_text_in_sentences(user_input))
+    #  print('User features shape:', user_features.shape)
+    #  author_verifier = LogisticRegression()
+
+    #  # we duplicate the user review to generate a balanced training data set
+    #  duplicated_user_features = scipy.sparse.vstack([user_features for _ in range(int(num_examples_per_class / user_features.shape[0]))])
+    #  print('Shape of duplicated user features:', duplicated_user_features.shape)
+
+    #  train_data = scipy.sparse.vstack([rand_review_features, duplicated_user_features])
+    #  print('Train data shape:', train_data.shape)
+
+    #  # Note: We use 0 for the generic author and 1 for the new author.
+    #  class_names = ['Generic-author', 'New-author']
+    #  train_labels = np.array([0] * num_examples_per_class + [1] * duplicated_user_features.shape[0])
+    #  print('Train labels shape:', train_labels.shape)
+
+    #  print('Training author verification model on data...')
+    #  #author_verifier.fit(train_data, train_labels)
+
+    #  # we create a pipeline for prediction and for the Lime Explainer
+    #  c = make_pipeline(vectorizer, clf)
+
+    #  print('Prediction on target document:', c.predict_proba([user_input])[0])
+    #  explainer = LimeTextExplainer(class_names=class_names)
+
+    #  # we use LIME to identify the features that are most indicative for both classes
+    #  num_lime_features = 100
+    #  exp = explainer.explain_instance(user_input, c.predict_proba, num_features=num_lime_features)
+    #  print(exp.as_list())
+
+    #  new_user_input = user_input
+    #  for (word, prob) in exp.as_list():
+    #    # we can now replace all features that have a positive probability (are indicative of teh New-author class)
+    #    # and observe how the probability of predicting the New-author changes
+    #    if prob > 0:
+    #      print('Replacing word %s...' % word)
+    #      new_user_input = new_user_input.replace(word, ' ')
+    #  print(c.predict_proba([new_user_input])[0])
+    #  sys.exit(0)
 
     return clf, vectorizer
+
+def save_pickles(clf, clf_fname, vectorizer, vectorizer_fname):
+    with timer('Saving pickles'):
+        joblib.dump(clf, clf_fname)
+        joblib.dump(vectorizer, vectorizer_fname)
+
+def load_pickles(clf_fname=None, vectorizer_fname=None):
+    if clf_fname == None:
+        clf_fname, vectorizer_fname = generate_pkl_filenames()
+
+    with timer('Loading pickles'):
+        clf = joblib.load(clf_fname)
+        vectorizer = joblib.load(vectorizer_fname)
+        return clf, vectorizer
+
+def classify_new_text(clf, vectorizer, new_body):
+    """Classify text
+    returns anonymized text, help message
+    """
+
+    with timer('Classifying new author'):
+
+        X = vectorizer.transform([new_body, ])
+        dfu = clf.decision_function(X)
+        print("decision function: %s" % dfu)
+        predicted = dfu[0]
+        p_min = min(predicted)
+        p_max = max(predicted)
+        delta = p_max - p_min
+        for p, name in zip(predicted, clf.classes_):
+            print("%f %s" % ((p - p_min) / delta, name))
+
+    dist = DistanceMetric.get_metric('euclidean')
+    avg = sum(predicted) / len(predicted)
+
+    jd = np.ones((1, len(predicted))) * avg
+    d = dist.pairwise(dfu, jd)[0][0]
+
+    jd = np.ones((1, len(predicted)))
+    d_one = dist.pairwise(dfu, jd)[0][0]
+
+    jd = np.ones((1, len(predicted))) * 0.0
+    d_zero = dist.pairwise(dfu, jd)[0][0]
+
+    return new_body, "Distances: %f %f" % (d_zero, d_one)
+
 
 def main():
 
@@ -155,32 +267,14 @@ def main():
     clf_fname, vectorizer_fname = generate_pkl_filenames()
     if opts.learn or not os.path.isfile(clf_fname):
         clf, vectorizer = learn()
-        with timer('Saving pickles'):
-            joblib.dump(clf, clf_fname)
-            joblib.dump(vectorizer, vectorizer_fname)
+        save_pickles(clf, clf_fname, vectorizer, vectorizer_fname)
 
     else:
-        with timer('Loading pickles'):
-            clf = joblib.load(clf_fname)
-            vectorizer = joblib.load(vectorizer_fname)
+        clf, vectorizer = load_pickles(clf_fname, vectorizer_fname)
 
-
-
-    delta = abs(clf._max_confidence - clf._min_confidence)
-
-    with timer('Classifying new author'):
-        with open('new_text') as f:
-            new_body = f.read()
-
-        X = vectorizer.transform([new_body, ])
-        dfu = clf.decision_function(X)
-        print("decision function: %s" % dfu)
-        predicted = clf.decision_function(X)[0]
-        p_min = min(predicted)
-        p_max = max(predicted)
-        delta = p_max - p_min
-        for p, name in zip(predicted, clf.classes_):
-            print("%f %s" % ((p - p_min) / delta, name))
+    with open('new_text') as f:
+        new_body = f.read()
+    classify_new_text(clf, vectorizer, new_body)
 
 
 if __name__ == '__main__':
--- src/webapp/webapp.py
+++ src/webapp/webapp.py
@@ -6,6 +6,10 @@
 from datetime import datetime
 from bottle import route, run, view, post, request
 
+import classifier
+
+clf = vectorizer = None
+
 
 # Bottle routes
 
@@ -18,13 +22,14 @@ def index():
 @view("index")
 def receive_form_post():
     orig_text = request.forms.get('text')
-    # FIXME
-    anonymized = orig_text
-    msg = "Better choose some synonims for: foo bar baz"
+    anonymized, msg = classifier.classify_new_text(clf, vectorizer, orig_text)
     return dict(orig_text=orig_text, anonymized=anonymized, msg=msg)
 
 
 def main():
+    global clf, vectorizer
+    clf, vectorizer = classifier.load_pickles()
+
     run(host='localhost', port=8080, debug=True)
 
 if __name__ == "__main__":
