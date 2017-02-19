3599b6d292f6e2aac1087445562332f19fa20abd
leok90 <leok90@gmail.com>
2017-02-19 15:34:57 +0000

    Made the default text a bit more distinctive

--- src/webapp/webapp.py
+++ src/webapp/webapp.py
@@ -18,7 +18,16 @@ from text_processing.style import analyze_string
 clf = vectorizer = None
 
 example_text = """
-Chuck Versus the Nemesis is the funniest action-packed episode of " Chuck " yet !	This episode takes place on Thanksgiving and Black Friday the next day so I'll just mention some tidbits here : With Bryce now revealed to be alive , Chuck gets in the interrogation room with him and has to prove he's the real one to Bryce by talking in Klingon which he's rusty at . At Ellie's Thanksgiving dinner , Anna shows jealousy at seeing Morgan's looks at Ellie as well as the way he's REALLY enjoying her food . Chuck also is envious watching secretly Sarah and Bryce ( who's unbeknownst to everyone else ) kissing in his bedroom , so much so he's tells Casey in encoded form about it . Chuck says " pineapple " to warn his fellow employees of an emergency as he's being held up by some enemy agents threatening to kill everyone in sight . Morgan awkwardly seeing Bryce and saying how he reminds him of Chuck's old roommate who he thinks is a " douche " . And then seeing Jeff hit on the head a couple of times after revealing how he liked being hit by a pineapple by his dad during his childhood . The episode ends with Sarah having to choose between two calls : One from Bryce on a classic antique phone made for mansions or on her ipod phone from Chuck . What to pick ? . . . Most hilarious and action-packed of " Chuck " yet ! With all my favorite characters and thousands of soccer mom shoppers contributing to the fun ! Very glad to know NBC gave this one a full-season pickup . Hope the writers ' strike ends before the episodes run out .
+CHUCK VERSUS THE NEMESIS is the funniest action-packed episode of " Chuck " yet !!!!	
+
+
+This episode takes place on Thanksgiving and Black Friday the next day so I'll just mention some tidbits here : With Bryce now revealed to be alive , Chuck gets in the interrogation room with him and has to prove he's the real one to Bryce by talking in Klingon which he's rusty at . At Ellie's Thanksgiving dinner , Anna shows jealousy at seeing Morgan's looks at Ellie as well as the way he's REALLY enjoying her food . Chuck also is envious watching secretly Sarah and Bryce ( who's unbeknownst to everyone else ) kissing in his bedroom , so much so he's tells Casey in encoded form about it ....
+
+ Chuck says " pineapple " to warn his fellow employees of an emergency as he's being held up by some enemy agents threatening to kill everyone in sight . Morgan awkwardly seeing Bryce and saying how he reminds him of Chuck's old roommate who he thinks is a " douche " . And then seeing Jeff hit on the head a couple of times after revealing how he liked being hit by a pineapple by his dad during his childhood . The episode ends with Sarah having to choose between two calls : One from Bryce on a classic antique phone made for mansions or on her ipod phone from Chuck . What to pick ? . . . Most hilarious and action-packed of " Chuck " yet ! With all my favorite characters and thousands of soccer mom shoppers contributing to the fun ! 
+
+
+Very glad to know NBC gave this one a full-season pickup . Hope the writers ' strike ends before the episodes run out ...
+
 """
 
 # Bottle routes
